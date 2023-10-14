af5bc0cf8fa11de27b3aab6de648ba150c6c718c
ZiqiXiao <99584387+ZiqiXiao@users.noreply.github.com>
2023-10-14 17:21:38 +0800

    Update

--- home/views.py
+++ home/views.py
@@ -22,15 +22,15 @@ class EbayBalanceView(View):
     def get(self, request):
         history = CheckingHistory.objects.only(
             'gift_card_no', 'balance', 'time')
-        formatted_history = [
-            {
-                'gift_card_no': item.gift_card_no,
-                'balance': item.balance,
-                'time': item.time.strftime('%Y-%m-%d %H:%M:%S')
-            }
-            for item in history
-        ]
-        history_json = json.dumps(formatted_history)
+        # formatted_history = [
+        #     {
+        #         'gift_card_no': item.gift_card_no,
+        #         'balance': item.balance,
+        #         'time': item.time.strftime('%Y-%m-%d %H:%M:%S')
+        #     }
+        #     for item in history
+        # ]
+        history_json = json.dumps(history)
         context = {
             'history': history_json,
         }
@@ -56,7 +56,8 @@ class EbayBalanceView(View):
             CheckingHistory.objects.create(
                 gift_card_no=gift_card_no,
                 balance=data['balance'],
-                # time=datetime.now(pytz.timezone('Asia/Shanghai')).strftime('%Y-%m-%d %H:%M:%S')
+                time=datetime.now(pytz.timezone('Asia/Shanghai')
+                                  ).strftime('%Y-%m-%d %H:%M:%S')
             )
             return HttpResponse('sucess', status=200)
 
